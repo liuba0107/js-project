12b90ebbd99ff7ddf43e4c2503d7c8124dffa043
liuba0107 <mama.liuba0107@gmail.com>
2025-02-07 23:10:32 +0800

    Create greet.js

---
+++ greet.js
@@ -0,0 +1,8 @@
+// Function to display a greeting
+function greet(name) {
+    return `Hello, ${name}! Welcome to the JavaScript project.`;
+}
+
+// Example usage
+const userName = "User";
+console.log(greet(userName));
